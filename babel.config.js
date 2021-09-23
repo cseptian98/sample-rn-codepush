4ff7de8bee36322e9528180db7d59140a9ce3d84
UT\cst_dev155 <cst_dev155@unitedtractors.com>
2021-09-23 09:09:56 +0700

    Fixing module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    'module-resolver',
-    {
-      root: ['./src'],
-      alias: {
-        test: './test',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          test: './test',
+        },
       },
-    },
+    ],
   ],
 };
