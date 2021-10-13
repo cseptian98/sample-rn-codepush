df283c09c50a03a5f8ff6ee67687d264a77cfcdb
Chandra Septian <cst_dev155@unitedtractors.com>
2021-10-13 11:45:57 +0700

    Update version and Favorite Screen

--- src/screen/Favorite.jsx
+++ src/screen/Favorite.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 
 class FavoriteScreen extends Component {
   render() {
     return (
-      <View>
-        <Text> Favorite Movies </Text>
+      <View style={styles.container}>
+        <Text style={styles.title}> Favorite Movies </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+  },
+});
 
 export default FavoriteScreen;
